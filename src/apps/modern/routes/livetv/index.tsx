2cfe360052f2b9a57d320fc8e375bcebe8632157
Kelvau <92541376+Kelvau@users.noreply.github.com>
2026-09-04 14:33:05 +0100

    feat: apply plex-style live tv shell

--- src/apps/modern/routes/livetv/index.tsx
+++ src/apps/modern/routes/livetv/index.tsx
@@ -2,10 +2,13 @@ import { CollectionType } from '@jellyfin/sdk/lib/generated-client/models/collec
 import React, { type FC } from 'react';
 
 import LibraryPage from 'apps/modern/features/libraries/components/LibraryPage';
+import MediaPageShell from 'components/media-ui/MediaPageShell';
 
 const LiveTv: FC = () => {
     return (
-        <LibraryPage type={CollectionType.Livetv} />
+        <MediaPageShell mode='livetv'>
+            <LibraryPage type={CollectionType.Livetv} />
+        </MediaPageShell>
     );
 };
 
